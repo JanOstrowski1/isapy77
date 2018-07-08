981b4654023e95d28d97e426c4863ae98749177a
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2018-07-08 15:25:14 +0200

    zadania

---
+++ Zadanie1.py
@@ -0,0 +1,5 @@
+wartosc = input("Podaj wartość stopni w skali Celcjusza: ")
+stopnie_c = float(wartosc)
+stopnie_f = stopnie_c * 9 / 5 + 32
+print("Podana temperatura w skali Fahrenheita wynosi : ")
+print(stopnie_f)
---
+++ Zadanie2.py
@@ -0,0 +1,5 @@
+wartosc = input("Podaj wartość stopni w skali Fahrenheita : ")
+stopnie_f = float(wartosc)
+stopnie_c = ( stopnie_f - 32 ) / 5 * 9
+print("Podana temperatura w skali Celcjusza wynosi : ")
+print(stopnie_c)
