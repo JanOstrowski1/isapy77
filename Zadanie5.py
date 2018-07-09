01f9d3816906914612c25f959851fade85943897
JanOstrowski1 <jan.ostrowski1337@gmail.com>
2018-07-09 21:00:12 +0200

    zadania

--- Zadanie5.py
+++ Zadanie5.py
@@ -7,4 +7,8 @@ dlugosc_b=input("Podaj długość boku b : ")
 wartosc_a=int(dlugosc_a)
 wartosc_b=int(dlugosc_b)
 print(wierzcholek+prosta*wartosc_a +wierzcholek )
- if(wartosc_b>1 , print(prosta_pionowa+puste_pole*wartosc_a+prosta_pionowa), wartosc_b= wartosc_b -1 )
+
+while wartosc_b> 1 :
+   print(prosta_pionowa+puste_pole*wartosc_a+prosta_pionowa)
+   wartosc_b-=1
+print(wierzcholek+prosta*wartosc_a +wierzcholek )
